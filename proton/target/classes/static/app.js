b62d809ddaab8779d1950ce7b938c43bcfdd81a0
Milan <mgvero94@gmail.com>
2018-01-08 20:48:44 +0100

    Sve stranice dodate

--- proton/target/classes/static/app.js
+++ proton/target/classes/static/app.js
@@ -19,10 +19,34 @@ angular.module('routerApp', [ 'ui.router', 'services', 'controllers' ]).config(
 				url : '/info',
 				templateUrl : 'pages/impressum.html',
 				controller : 'controller'
-			})
-			.state('basketball', {
+			}).state('basketball', {
 				url : '/basketball',
 				templateUrl : 'pages/basketball.html',
 				controller : 'controller'
+			}).state('football', {
+				url : '/football',
+				templateUrl : 'pages/football.html',
+				controller : 'controller'
+			}).state('frisbee', {
+				url : '/frisbee',
+				templateUrl : 'pages/frisbee.html',
+				controller : 'controller'
+			}).state('handball', {
+				url : '/handball',
+				templateUrl : 'pages/handball.html',
+				controller : 'controller'
+			}).state('beachvolleyball', {
+				url : '/beachvolleyball',
+				templateUrl : 'pages/bvolleyball.html',
+				controller : 'controller'
+			}).state('other1', {
+				url : '/other1',
+				templateUrl : 'pages/other1.html',
+				controller : 'controller'
+			}).state('other2', {
+				url : '/other2',
+				templateUrl : 'pages/other2.html',
+				controller : 'controller'
 			})
+			
 		});
