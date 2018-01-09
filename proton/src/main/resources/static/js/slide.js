62aa2ff994afff8dff9e7eabc502436e1145a411
nishikori94 <uvrnuto@gmail.com>
2018-01-09 17:06:36 +0100

    optimizovano sve

--- proton/src/main/resources/static/js/slide.js
+++ proton/src/main/resources/static/js/slide.js
@@ -28,3 +28,7 @@ function showSlides(n) {
 	slides[slideIndex - 1].style.display = "block";
 	dots[slideIndex - 1].className += " activeS";
 }
+
+$(".dropdown-item").on("click", function() {
+	$(".dropdown-menu").hide();
+});
